105bd720abb92dd912e362d111443aa102ce2f75
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-21 20:04:00 +0200

    feat(packages/ui): adding `ProductCard` component

---
+++ packages/ui/components/ProductCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="product-card">
+    <img :src="image" :alt="name" class="product-card__image" />
+    <h3 class="product-card__name">{{ name }}</h3>
+    <p class="product-card__id">ID: {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.product-card {
+  border: 1px solid #ccc;
+  padding: 16px;
+  border-radius: 4px;
+}
+
+.product-card__image {
+  width: 100%;
+  height: auto;
+  margin-bottom: 8px;
+  max-width: 150px;
+}
+
+.product-card__name {
+  margin-bottom: 4px;
+}
+
+.product-card__id {
+  font-size: 12px;
+  color: rgb(99, 99, 99);
+}
+</style>
