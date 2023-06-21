9bc86258eb2e06ecb46c030049e44ddda052132c
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-21 20:03:06 +0200

    chore(apps/shop): importing favicon with global style

--- apps/shop/nuxt.config.ts
+++ apps/shop/nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   modules: ['@packages/ui'],
   devtools: { enabled: true },
+  css: ['@/layouts/global.css'],
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }],
+    },
+  },
 })
