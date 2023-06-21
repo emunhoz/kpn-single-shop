a430d6218ec1be97df69e1b88b8a4308af3a5cd1
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-21 21:52:21 +0200

    feat(packages/ui): adding `header` component

---
+++ packages/ui/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <img
+        src="https://static.kpn.com/ssi/svg/kpn-logo.svg"
+        alt="Logo KPN"
+        width="77"
+        height="35"
+      />
+    </div>
+  </header>
+</template>
+<style scoped>
+.header {
+  width: 100%;
+  padding: 16px;
+  background: #fff;
+  margin-bottom: 40px;
+  position: sticky;
+  top: 0;
+}
+
+.header-wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
