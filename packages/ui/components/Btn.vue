a418fb3a8afe485340064adcc34460f27b0a496f
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-21 17:50:45 +0200

    feat(packages/ui): setting up `ui` component library

---
+++ packages/ui/components/Btn.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+      <slot></slot>
+  </button>
+</template>
+
+<style  scoped>
+button { 
+  border: none;
+  padding: 10px 20px;
+  background: blue;
+  color: white;
+  border-radius: 5px;
+}
+</style>
