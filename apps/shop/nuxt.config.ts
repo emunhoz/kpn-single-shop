43060469e1cc21244d5a39819fcd6a1ccff95a30
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-21 21:29:55 +0200

    chore(apps/shop): adding meta attributes

--- apps/shop/nuxt.config.ts
+++ apps/shop/nuxt.config.ts
@@ -8,6 +8,15 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en',
       },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'KPN single shop',
+        },
+      ],
       title: 'KPN single shop',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
