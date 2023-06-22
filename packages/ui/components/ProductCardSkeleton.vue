c78223090ad04815d5cd791d60f6cb59dc5a96d7
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-22 23:55:34 +0200

    feat(packages/ui): adding `ProductCardSkeleton` component

---
+++ packages/ui/components/ProductCardSkeleton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="product-card-skeleton">
+    <div class="image"></div>
+    <div class="name"></div>
+    <div class="id"></div>
+  </div>
+</template>
+
+<style>
+.product-card-skeleton {
+  background-color: #f0f0f0;
+  border-radius: 4px;
+  width: 100%;
+  height: 300px;
+  animation: skeleton-pulse 1s infinite alternate;
+}
+
+@keyframes skeleton-pulse {
+  0% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.image {
+  width: 100%;
+  height: 70%;
+  background-color: #ccc;
+}
+
+.name {
+  width: 80%;
+  height: 20px;
+  margin-top: 8px;
+  background-color: #ccc;
+}
+
+.id {
+  width: 60%;
+  height: 16px;
+  margin-top: 8px;
+  background-color: #ccc;
+}
+</style>
